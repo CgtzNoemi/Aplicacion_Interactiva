fb63994e5dc2c8bcac28e20494db4b9cc98859da
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2023-09-08 19:55:53 -0600

    2.6. El flujo de la interfaz

--- Aplicacion_Interactiva/Assets/Scripts/ColisionDetectar.cs
+++ Aplicacion_Interactiva/Assets/Scripts/ColisionDetectar.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class ColisionDetectar : MonoBehaviour
 {
+    public Animator objeto01Animator;
+    public Animator objeto02Animator;
+    public Animator objeto03Animator;
+
+    private bool isColliding = false; 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Tocando algo");
+        if (collision.CompareTag("ObjetoColision"))
+        {
+            Debug.Log("Tocando un objeto");
+
+            
+            objeto01Animator.SetBool("isFlying", true);
+            objeto02Animator.SetBool("isFlying", true);
+            objeto03Animator.SetBool("isFlying", true);
+
+            isColliding = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ObjetoColision"))
+        {
+            Debug.Log("Saliendo del objeto de colision");
+
+
+            objeto01Animator.SetBool("isFlying", false);
+            objeto02Animator.SetBool("isFlying", false);
+            objeto03Animator.SetBool("isFlying", false);
+
+            isColliding = false; 
+        }
     }
 }
